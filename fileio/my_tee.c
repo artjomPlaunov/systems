3515ab8714a5700e4606406ca5d2a09d05a47e1e
Artjom Plaunov <artyemnyc@gmail.com>
2024-08-11 17:10:37 -0400

    add -a flag for my_tee

--- fileio/my_tee.c
+++ fileio/my_tee.c
@@ -2,6 +2,10 @@
 // read standard input until end-of-file, writing a copy of the input 
 // to standard output and to the file named in the command line argument.
 
+// Default is to overwrite any existing file passed as an argument.
+// tee -a file will append to the file if it already exists (TODO).
+
+#include <ctype.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "tlpi_hdr.h"
@@ -10,19 +14,48 @@
 #define BUF_SIZE 1024
 #endif
 
+#define printable(ch) (isprint((unsigned char) ch) ? ch : '#')
+
+static void
+usageError(char *progName, char *msg, int opt) {
+  if (msg != NULL && opt != 0)
+    fprintf(stderr, "%s (-%c)\n", msg, printable(opt));
+  fprintf(stderr, "Usage: %s [-a] file\n", progName);
+  exit(EXIT_FAILURE);
+}
+
+
 
 int main(int argc, char *argv[]) {
+  // Process options.
+  int opt, afnd;
+  afnd = 0;
+  
+  while ((opt = getopt(argc, argv, "a")) != -1) {
+    switch (opt) {
+    case 'a': afnd = 1; break;
+    case '?': usageError(argv[0], "Unrecognized option", optopt); break;
+    default:  fatal("Unexpected case in switch()");
+    }
+  }
+
+  if (optind == argc)
+    usageError(argv[0], "", 0);
+		       
   int outputFd, openFlags;
   mode_t filePerms;
   ssize_t numRead;
   char buf[BUF_SIZE];
 
-  if (argc != 2 || strcmp(argv[1], "--help") == 0)
-    usageErr("%s tee file\n", argv[0]);
-
-  openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+  // If -a flag is found, set flag to append,
+  // otherwise set to truncate.
+  openFlags = O_CREAT | O_WRONLY;
+  if (afnd)
+    openFlags |= O_APPEND;
+  else
+    openFlags |= O_CREAT;
   filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
-  outputFd = open(argv[1], openFlags, filePerms);
+  outputFd = open(argv[optind], openFlags, filePerms);
   if (outputFd == -1)
     errExit("opening file %s", argv[1]);
 
